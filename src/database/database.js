7307324acba3ba0e341913e297d4795c2e2fdc4e
Pedro <pedrocastillo24.pc@gmail.com>
2022-08-09 10:01:49 -0300

    first commit

---
+++ src/database/database.js
@@ -0,0 +1,10 @@
+import Sequelize from "sequelize";
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+export const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`,
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
